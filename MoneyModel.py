763f718173b9182415461de475f7f1dc87f43b56
linne <alinnen94@googlemail.com>
2021-12-29 18:24:46 +0000

    Simple model complete.

--- MoneyModel.py
+++ MoneyModel.py
@@ -1,5 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+import matplotlib.pyplot as plt
 
 
 class MoneyAgent(Agent):
@@ -9,16 +10,47 @@ class MoneyAgent(Agent):
         self.wealth = 1
 
     def step(self):
-        # The agent's step will go here
-        pass
+        if self.wealth == 0:
+            return
+        other_agent = self.random.choice(self.model.schedule.agents)
+        other_agent.wealth += 1
+        self.wealth -= 1
 
 
 class MoneyModel(Model):
     """A model with some number of agents."""
     def __init__(self, N):
         self.num_agents = N
+        self.schedule = RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
+            self.schedule.add(a)
 
-test = 1
+    def step(self):
+        """Advance the model by one step."""
+        self.schedule.step()
+
+
+# # Run the model
+# model = MoneyModel(10)
+# for i in range(10):
+#     model.step()
+#
+# agent_wealth = [a.wealth for a in model.schedule.agents]
+# plt.hist(agent_wealth)
+# plt.show()
+
+all_wealth = []
+for j in range(100):
+    # Run the model
+    model = MoneyModel(10)
+    for i in range(10):
+        model.step()
+
+    # Store the results
+    for agent in model.schedule.agents:
+        all_wealth.append(agent.wealth)
+
+plt.hist(all_wealth, bins=range(max(all_wealth)+1))
+plt.show()
