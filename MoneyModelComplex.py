29af5db6ea2487cd2a4fe81adffe2af85bd79be5
linne <alinnen94@googlemail.com>
2021-12-30 12:43:26 +0000

    Complex model finished.

--- MoneyModelComplex.py
+++ MoneyModelComplex.py
@@ -1,8 +1,17 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+from mesa.batchrunner import BatchRunner
 import matplotlib.pyplot as plt
 import numpy as np
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum( xi * (N-i) for i,xi in enumerate(x) ) / (N*sum(x))
+    return (1 + (1/N) - 2*B)
 
 
 class MoneyAgent(Agent):
@@ -38,6 +47,7 @@ class MoneyModel(Model):
         self.num_agents = N
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
+        self.running = True
 
         # Create agents
         for i in range(self.num_agents):
@@ -49,21 +59,75 @@ class MoneyModel(Model):
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
 
+        self.datacollector = DataCollector(
+            model_reporters={"Gini": compute_gini},
+            agent_reporters={"Wealth": "wealth"})
+
     def step(self):
         """Advance the model by one step."""
+        self.datacollector.collect(self)
         self.schedule.step()
 
 
-# Run the model
-model = MoneyModel(50, 10, 10)
-for i in range(10):
-    model.step()
+# Batch run params
+fixed_params = {"width": 10,
+                "height": 10}
+variable_params = {"N": range(10, 500, 10)}
 
-agent_counts = np.zeros((model.grid.width, model.grid.height))
-for cell in model.grid.coord_iter():
-    cell_content, x, y = cell
-    agent_count = len(cell_content)
-    agent_counts[x][y] = agent_count
-plt.imshow(agent_counts, interpolation='nearest')
-plt.colorbar()
+batch_run = BatchRunner(MoneyModel,
+                        variable_params,
+                        fixed_params,
+                        iterations=5,
+                        max_steps=100,
+                        model_reporters={"Gini": compute_gini})
+batch_run.run_all()
+
+# Batch Gini plot
+run_data = batch_run.get_model_vars_dataframe()
+run_data.head()
+plt.scatter(run_data.N, run_data.Gini)
 plt.show()
+
+# Get the Agent DataCollection
+data_collector_agents = batch_run.get_collector_agents()
+print(data_collector_agents[(10,2)])
+
+# Get the Model DataCollection
+data_collector_model = batch_run.get_collector_model()
+print(data_collector_model[(10,1)])
+
+# Run the model
+# model = MoneyModel(50, 10, 10)
+# for i in range(100):
+#     model.step()
+
+# Get the Model Data
+
+# # Gini plot
+# gini = model.datacollector.get_model_vars_dataframe()
+# gini.plot()
+# plt.show()
+
+# # Agent wealth plot
+# agent_wealth = model.datacollector.get_agent_vars_dataframe()
+# agent_wealth.head()
+#
+# # All agents
+# end_wealth = agent_wealth.xs(99, level="Step")["Wealth"]
+# end_wealth.hist(bins=range(agent_wealth.Wealth.max()+1))
+# plt.show()
+#
+# # One agent (agent 14)
+# one_agent_wealth = agent_wealth.xs(14,level="AgentID")
+# one_agent_wealth.Wealth.plot()
+# plt.show()
+#
+# # Agent position plot
+# agent_counts = np.zeros((model.grid.width, model.grid.height))
+# for cell in model.grid.coord_iter():
+#     cell_content, x, y = cell
+#     agent_count = len(cell_content)
+#     agent_counts[x][y] = agent_count
+# plt.imshow(agent_counts, interpolation='nearest')
+# plt.colorbar()
+# plt.show()
